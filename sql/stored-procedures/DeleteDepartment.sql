a33fb87e72e77fdea96e20efd7e87e618f6c6c04
khaledahmed <khaled.ahmed333774@gmail.com>
2025-12-30 23:04:22 +0200

    fix: fix delete department to delete related branches also

--- sql/stored-procedures/DeleteDepartment.sql
+++ sql/stored-procedures/DeleteDepartment.sql
@@ -1,14 +1,26 @@
 CREATE PROCEDURE DeleteDepartment(@id INT)
 AS
 BEGIN
-	DELETE
-	FROM department
-	WHERE department_id = @id;
-	IF @@ROWCOUNT = 0
-	BEGIN
-		DECLARE @error_message VARCHAR(50) = CONCAT('Department with id ', @id, ' does not exist!');
-		THROW 50010, @error_message, 1;
-	END
+	BEGIN TRY
+		BEGIN TRANSACTION
+			DELETE 
+			FROM branch_department
+			WHERE department_id = @id;
+			DELETE
+			FROM department
+			WHERE department_id = @id;
+			IF @@ROWCOUNT = 0
+			BEGIN
+				DECLARE @error_message VARCHAR(50) = CONCAT('Department with id ', @id, ' does not exist!');
+				THROW 50010, @error_message, 1;
+			END
+		COMMIT TRANSACTION;
+	END TRY
+	BEGIN CATCH
+		IF @@TRANCOUNT > 0
+			ROLLBACK TRANSACTION;
+		THROW;
+	END CATCH
 END
 
-EXEC DeleteDepartment 2
+EXEC DeleteDepartment 1
