4f83f75b6779a1e897b7f78421645de35eda7cde
TryOmar <TryOmarAbbas@gmail.com>
2025-12-31 21:34:59 +0200

    feat: add topic CRUD stored procedures

---
+++ sql/stored-procedures/DeleteTopic.sql
@@ -0,0 +1,28 @@
+CREATE OR ALTER PROCEDURE DeleteTopic
+    @topic_id INT
+AS
+BEGIN
+    BEGIN TRY
+        BEGIN TRANSACTION
+            IF EXISTS (SELECT 1 FROM topic WHERE topic_id = @topic_id)
+            BEGIN
+                -- Delete dependencies
+                DELETE FROM course_questions_on_topic WHERE topic_id = @topic_id;
+
+                -- Delete topic
+                DELETE FROM topic WHERE topic_id = @topic_id;
+            END
+            ELSE
+            BEGIN
+                DECLARE @error_message VARCHAR(50) = 'Topic with ID ' + CAST(@topic_id AS VARCHAR(20)) + ' does not exist!';
+                THROW 50006, @error_message, 1;
+            END
+        COMMIT TRANSACTION;
+    END TRY
+    BEGIN CATCH
+        IF @@TRANCOUNT > 0
+            ROLLBACK TRANSACTION;
+        THROW;
+    END CATCH
+END
+GO
---
+++ sql/stored-procedures/SelectTopic.sql
@@ -0,0 +1,9 @@
+CREATE OR ALTER PROCEDURE SelectTopic
+    @topic_id INT = NULL
+AS
+BEGIN
+    SELECT topic_id, topic_order, topic_duration, topic_title
+    FROM topic
+    WHERE @topic_id IS NULL OR topic_id = @topic_id;
+END
+GO
