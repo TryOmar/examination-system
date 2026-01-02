bfc376cc733ebfa493ffee5d54fdf61b63f6e9cc
Elaraby218 <152742814+Elaraby218@users.noreply.github.com>
2026-01-02 18:33:54 +0200

    fix: dynamically protect True/False choices in UpdateChoiceText procedure

--- sql/stored-procedures/Question Management/UpdateChoiceText.sql
+++ sql/stored-procedures/Question Management/UpdateChoiceText.sql
@@ -14,7 +14,15 @@ BEGIN
     IF NOT EXISTS (SELECT 1 FROM quesiton_choice WHERE choice_id = @ChoiceID)
         RETURN -3;
 
-    IF @ChoiceID IN (1, 2)
+    -- Protect global True/False choice entries (lookup dynamically)
+    DECLARE @TrueChoiceID INT;
+    DECLARE @FalseChoiceID INT;
+
+    SELECT @TrueChoiceID = choice_id FROM quesiton_choice WHERE LOWER(choice_text) = 'true';
+    SELECT @FalseChoiceID = choice_id FROM quesiton_choice WHERE LOWER(choice_text) = 'false';
+
+    IF (@TrueChoiceID IS NOT NULL AND @ChoiceID = @TrueChoiceID) OR
+       (@FalseChoiceID IS NOT NULL AND @ChoiceID = @FalseChoiceID)
         RETURN -4;
 
     BEGIN TRY
