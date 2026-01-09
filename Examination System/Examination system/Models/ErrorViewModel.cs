9123350605b8648fd9cb9c96bfa715e5bc9f0888
AZIZ20035 <medo.mohamed.2003.2016@gmail.com>
2026-01-09 18:52:43 +0200

    Edit the last Commite (sorry)

--- Examination System/Examination system/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Examination_system.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
